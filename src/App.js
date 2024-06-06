4fe6d1f72ce2b15208e8ed3321da760a742aa430
Abirami902 <abiramipabi2001@gmail.com>
2024-06-06 11:33:35 +0530

    commit 1

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Movies from './Movies';
+import Detail from './Detail';
+// import Home from './Home';
+// import FetchData from './FetchData';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<App />} /> */}
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        {/* <Route path="/home" element={<Home />} />
+        <Route path="/fetchdata" element={<FetchData />} /> */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
+
+
+export default App;
